6ddf4c9a2511d47dc1f14d227ba734ece6567b02
Ray Messina <rmessina10@gmail.com>
2021-02-03 23:29:53 -0600

    Mongoose Part 3

--- index.js
+++ index.js
@@ -5,22 +5,41 @@ const url = 'mongodb://localhost:27017/nucampsite';
 
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 connect.then(() => {
+
     console.log('Connected correctly to server.');
 
     Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
-    }).then(campsite => {
-        console.log(campsite);
-        return Campsite.find();
     })
-        .then(campsites => {
-            console.log(campsites);
+        .then(campsite => {
+            console.log(campsite);
+
+            return Campsite.findByIdAndUpdate(campsite._id, {
+                $set: { description: 'Updated Test Document' }
+            }, {
+                new: true
+            });
+        })
+        .then(campsite => {
+            console.log(campsite.comments);
+
+            campsite.comments.push({
+                rating: 5,
+                text: 'What a magnificent view!',
+                author: 'Tinus Lorvaldes'
+            });
+
+            return campsite.save();
+        })
+        .then(campsite => {
+            console.log(campsite);
             return Campsite.deleteMany();
 
         })
